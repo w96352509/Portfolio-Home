831f81733138763f48c3b16dd052ffb692f4aac8
vic <vichsu38@gmail.com>
2021-11-21 22:47:22 +0800

    s

---
+++ SpringBoot-Portfolio-20211121-home/src/test/java/com/example/demo/SpringBootPortfolio20211121HomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPortfolio20211121HomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
