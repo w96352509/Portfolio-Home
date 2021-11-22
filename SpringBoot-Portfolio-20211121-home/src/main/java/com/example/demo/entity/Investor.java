e1726b18a2aebbf656fba8389f782c18197c2666
vic <vichsu38@gmail.com>
2021-11-22 16:26:31 +0800

    ss

--- SpringBoot-Portfolio-20211121-home/src/main/java/com/example/demo/entity/Investor.java
+++ SpringBoot-Portfolio-20211121-home/src/main/java/com/example/demo/entity/Investor.java
@@ -10,10 +10,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.Table;
 
 @Entity
-@Table
 public class Investor {
 
 	@Id
